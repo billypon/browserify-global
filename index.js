1e5c0d617782f969c947a4d68150c2a80d245b4c
billypon <billypon@gmail.com>
2018-01-18 16:23:41 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var concat = require('concat-stream');
+
+module.exports = function (b, opts) {
+    var bundle = b.bundle;
+    b.bundle = function (cb) {
+        var output = bundle.call(b);
+        output.on('error', cb);
+        output.pipe(concat(function (body) {
+            var src = body.toString();
+            src = src.replace(new RegExp('([\'"])' + (b.basedir || process.cwd()), 'g'), '__dirname + $1');
+            cb(null, new Buffer(src));
+        }));
+    }
+}
