5c272abc28f3cd54df345e5fac710ad9260080e4
billypon <billypon@gmail.com>
2018-01-19 11:10:05 +0800

    no longer modify insert-module-globals' default vars

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 var path = require('path');
-var insert = require('insert-module-globals')
-
-var basedir;
-
-insert.vars.__dirname = function (file) {
-    var dirname = path.relative(basedir, path.dirname(file));
-    return '__dirname' + (dirname ? '+' + JSON.stringify(dirname) : dirname);
-}
-
-insert.vars.__filename = function (file) {
-    var filename = path.relative(basedir, file);
-    return '__dirname' + (filename ? '+' +JSON.stringify('/' + filename) : filename);
-}
+var insertGlobals = require('insert-module-globals');
 
 module.exports = function (b, opts) {
-    basedir = opts.basedir || process.cwd();
+    var basedir = opts.basedir || process.cwd();
+    var insertGlobalVars = b._options.insertGlobalVars || {};
+
+    for (var x in insertGlobals.vars) {
+        insertGlobalVars[x] = undefined;
+    }
+    insertGlobalVars.__dirname = function (file) {
+        var dirname = path.relative(basedir, path.dirname(file));
+        return '__dirname' + (dirname ? '+' + JSON.stringify(dirname) : dirname);
+    },
+    insertGlobalVars.__filename = function (file) {
+        var filename = path.relative(basedir, file);
+        return '__dirname' + (filename ? '+' +JSON.stringify('/' + filename) : filename);
+    }
+
+    b._options.insertGlobalVars = b._mdeps.options.insertGlobalVars = insertGlobalVars;
 }
